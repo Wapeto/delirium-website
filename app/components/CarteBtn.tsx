3581ab0b46d042d29ea57de0821041cea4665dd0
Wapeto <dupetopetoman@gmail.com>
2025-01-23 18:44:43 +0100

    Add CarteBtn

---
+++ app/components/CarteBtn.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import React from "react";
+import { MdOutlineMenuBook } from "react-icons/md";
+
+const CarteBtn = () => {
+  return (
+    <Link
+      href="https://menu.eazee-link.com/?o=q&id=DHTLU688JL"
+      className="bg-rose w-[180px] h-[63px] px-2 py-1 rounded-lg shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] justify-center items-center gap-2 inline-flex"
+    >
+      <MdOutlineMenuBook className="w-12 h-12 relative"/>
+      <div className="text-center text-[#fcefef] text-4xl font-black font-titillium">
+        CARTE
+      </div>
+    </Link>
+  );
+};
+
+export default CarteBtn;
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
+import CarteBtn from "./components/CarteBtn";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="text-3xl font-bold">Welcome to the new Délirium Café Strasbourg&apos;s website</h1>
+      <CarteBtn />
     </main>
   );
 }
