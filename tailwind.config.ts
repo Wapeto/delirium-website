a217d08019b08195ed70e9a1d3e830b2a8923951
Wapeto <dupetopetoman@gmail.com>
2025-01-23 18:18:23 +0100

    Add color palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,23 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        rose : {
+          DEFAULT: '#F193A2',
+          light: '#EBAAB4',
+          dark: '#B7727D'
+        },
+        bleu : {
+          DEFAULT: '#22AED1',
+          light: '#33AFCE',
+          dark: '#1F99B7'
+        },
+        background: '#28262C',
+        beige : '#FCEFEF'
+      },
+      fontFamily: {
+        titillium: ["var(--font-titillium-web)", "sans-serif"],
+        geistSans: ["var(--font-geist-sans)", "sans-serif"],
+        geistMono: ["var(--font-geist-mono)", "monospace"],
       },
     },
   },
