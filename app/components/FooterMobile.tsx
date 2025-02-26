cce75f2087e3defeb0fc0c4ebc86602cfe990135
Caslol974 <85448272+Caslol974@users.noreply.github.com>
2025-02-26 16:47:17 +0100

    Update FooterMobile component: adjust text styles and enhance link structure for improved accessibility

--- app/components/FooterMobile.tsx
+++ app/components/FooterMobile.tsx
@@ -8,7 +8,7 @@ import { HiOutlineMapPin } from "react-icons/hi2";
 
 const FooterMobile = () => {
   return (
-    <footer className="flex flex-col items-center justify-center gap-4 mt-4 bg-bleu w-full text-base text-background">
+    <footer className="flex flex-col items-center justify-center gap-4 mt-4 bg-bleu w-full text-background font-titillium text-sm">
       <address className="flex justify-center text-4xl w-80">
         <Link
           href="https://www.facebook.com/deliriumstrasbourg/"
@@ -39,11 +39,20 @@ const FooterMobile = () => {
           <IconFooterMail />
         </Link>
       </address>
-      <div className="flex items-center justify-center w-72 border border-background rounded-lg pt-2 pb-2">
+      <Link
+        href={
+          "https://www.google.com/maps/place//data=!4m2!3m1!1s0x4796c84c1d430bf1:0x6d86f9bc1635704d?sa=X&ved=1t:8290&ictx=111"
+        }
+        className="flex items-center justify-center w-72 border border-background rounded-lg pt-2 pb-2"
+      >
         <HiOutlineMapPin className="text-4xl" />
-        <p className="m-auto">5 Quai de Paris, 67000 Strasbourg</p>
-      </div>
-      <p className="text-background">© 2021 - Tous droits réservés</p>
+        <p className="m-auto font-semibold">
+          5 Quai de Paris, 67000 Strasbourg
+        </p>
+      </Link>
+      <p className="text-background font-bold">
+        © 2025 Délirium Café Strasbourg - Tous droits réservés
+      </p>
     </footer>
   );
 };
